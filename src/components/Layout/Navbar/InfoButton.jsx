a4b0ab392fc3bae4c597c0f6dd19520029e18763
Divij Katyal <divijkatyal@gmail.com>
2023-11-12 15:06:38 +0530

    info-add

---
+++ src/components/Layout/Navbar/InfoButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function InfoButton() {
+  return (
+    <a href="https://github.com/Divijkatyal0406/warewe-backend/#api-endpoints">
+    <button className='mr-auto py-4 px-4 pr-0 text-black bg-tranparent rounded'>
+        ℹ️ Info
+    </button>
+    </a>
+  );
+}
--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
-// import './navbar.css'
+import InfoButton from './InfoButton';
 
 export default function Navbar() {
   return (
-    <div className='absolute w-full border-t-4 border-orange-500 bg-white '>
-      <div className='border-b border-gray-300'>
-        <div className='flex max-w-6xl mx-auto'>
-          <span className='py-4 px-4 text-2xl font-semibold text-orange-500'>
+    <div className='absolute w-full border-t-4 border-orange-500 bg-white'>
+      <div className='border-b border-gray-300 flex justify-between max-w-6xl mx-auto'>
+        <span className='py-4 px-4 text-2xl font-semibold text-orange-500'>
           Warewe Consultancy Private Limited Assignment - Divij Katyal
-          </span>
-        </div>
+        </span>
+        <InfoButton />
       </div>
     </div>
   );
